a0fbe9ec2f7748f89ed5ce410b8787910cdc0ab2
GITlibin1025 <libin1025@aliyun.com>
2018-08-23 08:09:03 +0800

    简单的数字游戏

---
+++ num.py
@@ -0,0 +1,13 @@
+#这是我的第一个程序
+#Notepad++,Ctrl+S,保存
+print ('--------游戏--------')
+print ("猜猜我心里想的数字几")
+temp = input()
+guess = int(temp)
+if guess == 8:
+	print ("WTF,这你都能猜到")
+	print ("不过猜中了也没奖励，哈哈哈")
+else:
+	print ("没猜到吧，我想的是8")
+print ("游戏结束不玩了^-^||")
+
