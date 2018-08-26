3dad638be8573076f963e82c86ce9947ce20283e
GITlibin1025 <libin1025@aliyun.com>
2018-08-26 21:51:20 +0800

    字符串转列表

---
+++ str_list.py
@@ -0,0 +1,8 @@
+
+写一个函数get_digits(n)，将参数n分解出每个位的数字并按顺序存放到列表中。举例：get_digits(12345) ==> [1, 2, 3, 4, 5]
+def get_digits(x):
+	list0 = list(x)
+	print (list0)
+	
+a = input('字符串分解列表，请输入')
+get_digits(a)
